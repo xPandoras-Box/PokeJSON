b6ef45a212b259f25957bb1cd1ce9877f437ca81
Francesca G <75336760+xPandoras-Box@users.noreply.github.com>
2022-04-01 08:22:45 -0700

    Add files via upload

---
+++ pokeJson/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace pokeJson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastButton = new System.Windows.Forms.Button();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.desTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.moveTextbox = new System.Windows.Forms.TextBox();
+            this.typeTextbox = new System.Windows.Forms.TextBox();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.hpNumeric = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hpNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lastButton
+            // 
+            this.lastButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastButton.Location = new System.Drawing.Point(728, 237);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new System.Drawing.Size(65, 33);
+            this.lastButton.TabIndex = 40;
+            this.lastButton.Text = ">>";
+            this.lastButton.UseVisualStyleBackColor = true;
+            this.lastButton.Click += new System.EventHandler(this.lastButton_Click);
+            // 
+            // firstButton
+            // 
+            this.firstButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstButton.Location = new System.Drawing.Point(8, 237);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(65, 33);
+            this.firstButton.TabIndex = 39;
+            this.firstButton.Text = "<<";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(728, 189);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(65, 33);
+            this.nextButton.TabIndex = 38;
+            this.nextButton.Text = ">";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // prevButton
+            // 
+            this.prevButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prevButton.Location = new System.Drawing.Point(8, 189);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(65, 33);
+            this.prevButton.TabIndex = 37;
+            this.prevButton.Text = "<";
+            this.prevButton.UseVisualStyleBackColor = true;
+            this.prevButton.Click += new System.EventHandler(this.prevButton_Click);
+            // 
+            // delButton
+            // 
+            this.delButton.Location = new System.Drawing.Point(79, 389);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(103, 33);
+            this.delButton.TabIndex = 36;
+            this.delButton.Text = "Delete";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(325, 389);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(103, 33);
+            this.addButton.TabIndex = 35;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // desTextbox
+            // 
+            this.desTextbox.Location = new System.Drawing.Point(448, 314);
+            this.desTextbox.Multiline = true;
+            this.desTextbox.Name = "desTextbox";
+            this.desTextbox.Size = new System.Drawing.Size(302, 85);
+            this.desTextbox.TabIndex = 34;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(444, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 23);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Description:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(201, 389);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(103, 33);
+            this.saveButton.TabIndex = 32;
+            this.saveButton.Text = "Update";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // moveTextbox
+            // 
+            this.moveTextbox.Location = new System.Drawing.Point(559, 169);
+            this.moveTextbox.Name = "moveTextbox";
+            this.moveTextbox.Size = new System.Drawing.Size(141, 20);
+            this.moveTextbox.TabIndex = 30;
+            // 
+            // typeTextbox
+            // 
+            this.typeTextbox.Location = new System.Drawing.Point(559, 101);
+            this.typeTextbox.Name = "typeTextbox";
+            this.typeTextbox.Size = new System.Drawing.Size(141, 20);
+            this.typeTextbox.TabIndex = 29;
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(559, 31);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(141, 20);
+            this.nameTextbox.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(444, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 23);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "HP:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(444, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 23);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Move:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(444, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 23);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Type:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(79, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(444, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 23);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Name:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // hpNumeric
+            // 
+            this.hpNumeric.Location = new System.Drawing.Point(559, 235);
+            this.hpNumeric.Name = "hpNumeric";
+            this.hpNumeric.Size = new System.Drawing.Size(120, 20);
+            this.hpNumeric.TabIndex = 41;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hpNumeric);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.firstButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.prevButton);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.desTextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.moveTextbox);
+            this.Controls.Add(this.typeTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hpNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lastButton;
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox desTextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox moveTextbox;
+        private System.Windows.Forms.TextBox typeTextbox;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown hpNumeric;
+    }
+}
+
---
+++ pokeJson/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections;
+using System.IO;
+
+namespace pokeJson
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList pokes = new ArrayList();
+        int current = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader inFile = File.OpenText("pokemon.json");
+            while (inFile.Peek() != -1)
+            {
+                string p = inFile.ReadLine();
+                Pokemon pString = JsonSerializer.Deserialize<Pokemon>(p);
+                pokes.Add(pString);
+            }
+            
+            show();
+        }
+
+        public void show()
+        {
+            if (pokes.Count > 0)
+            {
+            Pokemon p = (Pokemon)pokes[current];
+            nameTextbox.Text = p.Name;
+            moveTextbox.Text = p.Move;
+            typeTextbox.Text = p.Type;
+            hpNumeric.Value = p.HP;
+            desTextbox.Text = p.Desc;
+            if (File.Exists(p.Image))
+            {
+                pictureBox1.ImageLocation = p.Image;
+
+            }
+            
+
+            }
+            
+        }
+
+        public void save()
+        {
+            var entry = new Pokemon
+            {
+                Name = nameTextbox.Text,
+                Move = moveTextbox.Text,
+                Type = typeTextbox.Text,
+                HP = (int)hpNumeric.Value,
+                Desc = desTextbox.Text,
+                Image = pictureBox1.ImageLocation
+
+            };
+            String pokemonString = JsonSerializer.Serialize(entry);
+            pokes.Add(entry);
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.ShowDialog();
+            if (System.IO.File.Exists(openFileDialog1.FileName))
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            save();
+            StreamWriter outFile = File.CreateText("pokemon.json");
+            foreach (var item in pokes)
+            {
+                outFile.WriteLine(JsonSerializer.Serialize(item));
+            }
+            outFile.Close();
+           
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (pokes.Count == current + 1)
+            {
+                current = 0;
+            }
+            else if (current < pokes.Count)
+            {
+                current++;
+            }
+            show();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            nameTextbox.Text = null;
+            desTextbox.Text = null;
+            hpNumeric.Text = null;
+            moveTextbox.Text = null;
+            typeTextbox.Text = null;
+            pictureBox1.Image = null;
+        }
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            if (current == 0)
+            {
+                current = pokes.Count - 1;
+            }
+            else if (current > 0)
+            {
+                current--;
+            }
+            show();
+        }
+
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            current = 0;
+            show();
+        }
+
+        private void lastButton_Click(object sender, EventArgs e)
+        {
+            current = pokes.Count - 1;
+            show();
+        }
+
+        private void delButton_Click(object sender, EventArgs e)
+        {
+            pokes.Remove(pokes[current]);
+            show();
+        }
+    }
+    }
+
+
+
+
